327657bd4dad796dcf7ffb032efd4ee1547f1633
Maricel Tagle <maricel.tagle@asurion.com>
2019-03-06 14:34:56 +0800

    Second commit

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+<<<<<<< Updated upstream
 from flask import request
+=======
+>>>>>>> Stashed changes
 import json
 
 app = Flask(__name__)
@@ -8,7 +11,7 @@ app = Flask(__name__)
 def index():
     return "This is my homepage"
 
-user_list = []
+user_list = [] #add users here
 # GET http://localhost:5000/v1/users/id?param=name
 @app.route("/v1/users/<id>", methods=["GET"])
 def users(id):
@@ -24,6 +27,3 @@ def users(id):
         else:
           return "User details `{}`: {}".format(param, data)
   return "User not found"
-
-
-  
