450adbf3ab77c3dd9efac31550ca5dbda1f0473c
Vonn Bareng <vonnharvey61@gmail.com>
2019-07-05 08:53:43 +0800

    fibo -> get(n)

--- fibo.py
+++ fibo.py
@@ -1,8 +1,21 @@
 def get(n):
-	return n
+        n = n + 1
+        a = 0
+        b = 1
+        if n == 0: 
+                return a 
+        elif n == 1: 
+                return b 
+        else: 
+                for i in range(2,n): 
+                    c = a + b 
+                    a = b 
+                    b = c 
+                return b
+
 
 def main():
-	print('HELLO WORLD!')	
+	print(get(7))	
 
 if __name__ == '__main__':
 	main()
