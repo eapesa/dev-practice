f594f4d998e295371cc55ffe7371a0fbdf4a1251
Vonn Bareng <vonnharvey61@gmail.com>
2019-07-05 09:39:25 +0800

    calculator

---
+++ calculator.py
@@ -0,0 +1,22 @@
+def add(a, b):
+	c = a + b
+	return c
+
+def sub(a, b):
+	c = a - b
+	return c
+	
+def multi(a, b):
+	c = a * b
+	return c
+	
+def div(a, b):
+	c = a / b
+	return c
+	
+def main():
+	print(sub(2,2))
+
+if __name__ == '__main__':
+# execute only if run as the entry point into the program
+    main()
